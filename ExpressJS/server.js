5b8f7f7d114d1b332e138ce5ebe594edb7c2e0c7
Tung Phan <thanhtungphanvu@gmail.com>
2024-05-22 13:06:17 +0300

    get request

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
-
+const friends = [
+  {
+    id: 0,
+    name: "Max",
+  },
+  {
+    id: 1,
+    name: "John",
+  },
+];
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = +req.params.friendId;
+  const friend = friends[friendId];
+  if (friend) {
+    res.json(friend);
+  } else {
+    res.status(404).json({ error: "friend not found" });
+  }
 });
 app.get("/message", (req, res) => {
   res.send("<ul>Heeello</ul>");
