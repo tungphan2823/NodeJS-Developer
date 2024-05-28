3bf9c866cf6a15cad1936adc1fae4d97d337a12e
Tung Phan <thanhtungphanvu@gmail.com>
2024-05-28 18:02:22 +0300

    image send

--- ExpressJS/controllers/messages.controller.js
+++ ExpressJS/controllers/messages.controller.js
@@ -1,5 +1,7 @@
+const path = require("path");
 function getMessages(req, res) {
-  res.send("<ul>Heeello</ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
+  // res.send("<ul>Heeello</ul>");
 }
 function postMessages(req, res) {
   console.log("Updating");
