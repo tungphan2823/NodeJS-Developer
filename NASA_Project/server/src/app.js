20ce3c9c691fc17368a7469eff2f8174bcb6628e
Tung Phan <thanhtungphanvu@gmail.com>
2024-05-29 19:47:22 +0300

    getAllPlanets

--- NASA_Project/server/src/app.js
+++ NASA_Project/server/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const planetRouter = require("./routes/planet//planets.router");
+
 const app = express();
 
 app.use(express.json());
 
+app.use(planetRouter);
 
 module.exports = app;
