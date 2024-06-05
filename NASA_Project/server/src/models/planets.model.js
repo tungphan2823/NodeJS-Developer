76d523b71bcf83c289a8635b67f15bd38fe8ef2f
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-05 18:33:47 +0300

    loading data on startup csv file

--- NASA_Project/server/src/models/planets.model.js
+++ NASA_Project/server/src/models/planets.model.js
@@ -1,2 +1,43 @@
-const planets = [];
-module.exports = planets;
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const habitablePlanet = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+function loadPlanetData() {
+  return new Promise(function (resolve, reject) {
+    fs.createReadStream("kepler_data.csv")
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanet.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanet.length} habitable planets found`);
+        resolve();
+      });
+  });
+}
+
+// parse();
+module.exports = {
+  loadPlanetData,
+  planets: habitablePlanet,
+};
--- NASA_Project/server/src/server.js
+++ NASA_Project/server/src/server.js
@@ -2,6 +2,11 @@ const app = require("./app");
 const http = require("http");
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+const { loadPlanetData } = require("./models/planets.model");
+async function startServer() {
+  await loadPlanetData();
+  server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+  });
+}
+startServer()
