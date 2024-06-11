d7a7ce9a6a3ca6064c0a096ebd58f0329c1f8f00
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-11 21:30:59 +0300

    complete testing

--- NASA_Project/server/src/routes/launches/launches.test.js
+++ NASA_Project/server/src/routes/launches/launches.test.js
@@ -10,7 +10,57 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launch", () => {
-  test("It should response with 200 success", () => {});
-  test("It should catch missing require properties", () => {});
-  test("It should catch invalid date", () => {});
+  const completeLaunchData = {
+    mission: "test",
+    rocket: "test",
+    target: "test",
+    launchDate: "January 4, 2015",
+  };
+  const launchDataWithInvalidDate = {
+    mission: "test",
+    rocket: "test",
+    target: "test",
+    launchDate: "zoots",
+  };
+  const launchDataWithoutDate = {
+    mission: "test",
+    rocket: "test",
+    target: "test",
+  };
+  test("It should response with 201 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It should catch missing require properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "mission, rocket, launchDate and destination are required",
+    });
+  });
+
+  test("It should catch invalid date", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "launchDate must be a valid date",
+    });
+  });
 });
