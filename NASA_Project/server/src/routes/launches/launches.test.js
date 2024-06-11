979da3acd071eb0b94af1e0888fc63817bbfd404
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-11 20:34:10 +0300

    add test

---
+++ NASA_Project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("It should response with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("It should response with 200 success", () => {});
+  test("It should catch missing require properties", () => {});
+  test("It should catch invalid date", () => {});
+});
