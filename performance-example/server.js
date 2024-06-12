34c2b2667ab6bfddf15ec518202ab3f03be4ca48
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-12 18:14:30 +0300

    update

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const cluster = require('cluster');
-
+const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
 const app = express();
 
 function delay(duration) {
   const startTime = Date.now();
-  while(Date.now() - startTime < duration) {
+  while (Date.now() - startTime < duration) {
     //event loop is blocked...
   }
 }
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // JSON.stringify({}) => "{}"
   // JSON.parse("{}") => {}
   // [5,1,2,3,4].sort()
   res.send(`Performance example: ${process.pid}`);
 });
 
-app.get('/timer', (req, res) => {
+app.get("/timer", (req, res) => {
   delay(9000);
   res.send(`Ding ding ding! ${process.pid}`);
 });
 
-console.log('Running server.js...');
+console.log("Running server.js...");
 if (cluster.isMaster) {
-  console.log('Master has been started...');
-  cluster.fork();
-  cluster.fork();
+  console.log("Master has been started...");
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i <= NUM_WORKERS; i++) {
+    cluster.fork();
+  }
 } else {
-  console.log('Worker process started.');
+  console.log("Worker process started.");
   app.listen(3000);
 }
