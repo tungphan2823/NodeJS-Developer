b951bac249176a8a36cfd7f49d5454c77c5e7737
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-13 02:03:46 +0300

    using mongodb atlas

--- NASA_Project/server/src/server.js
+++ NASA_Project/server/src/server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
 const http = require("http");
+
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:Fawmf6XLFiO0R7fX@nasacluster.mny8tcq.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
+
 const server = http.createServer(app);
 const { loadPlanetData } = require("./models/planets.model");
 async function startServer() {
@@ -9,4 +16,4 @@ async function startServer() {
     console.log(`Server running at http://localhost:${PORT}/`);
   });
 }
-startServer()
+startServer();
