a9d069d7227b1227dacaec24a9c1bf9d1a1250e2
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-15 21:22:02 +0300

    connect to MongoDB

--- NASA_Project/server/src/server.js
+++ NASA_Project/server/src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const http = require("http");
-
+const { loadPlanetData } = require("./models/planets.model");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8000;
@@ -9,8 +9,15 @@ const MONGO_URL =
   "mongodb+srv://nasa-api:Fawmf6XLFiO0R7fX@nasacluster.mny8tcq.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
 
 const server = http.createServer(app);
-const { loadPlanetData } = require("./models/planets.model");
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetData();
   server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
