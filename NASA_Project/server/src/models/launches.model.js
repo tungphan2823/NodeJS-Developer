cc33f288f75c02359d3bbb0be8ee55b5048b13d5
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-24 00:17:35 +0300

    sort data

--- NASA_Project/server/src/models/launches.model.js
+++ NASA_Project/server/src/models/launches.model.js
@@ -5,17 +5,17 @@ const launches = new Map();
 const DEFAULT_FLIGHT_NUMBER = 100;
 let latestFlightNumber = 100;
 
-const launch = {
-  flightNumber: 100,
-  mission: "Kepler Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-442 b",
-  customer: ["Tung", "NASA"],
-  upcoming: true,
-  success: true,
-};
-saveLaunch(launch);
+// const launch = {
+//   flightNumber: 100,
+//   mission: "Kepler Exploration X",
+//   rocket: "Explorer IS1",
+//   launchDate: new Date("December 27, 2030"),
+//   target: "Kepler-442 b",
+//   customer: ["Tung", "NASA"],
+//   upcoming: true,
+//   success: true,
+// };
+// saveLaunch(launch);
 // launches.set(launch.flightNumber, launch);
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
@@ -97,6 +97,7 @@ async function getAllLaunches(skip, limit) {
         __v: 0,
       }
     )
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
 }
