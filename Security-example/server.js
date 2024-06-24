1a29c55e2b4db14af114717a15b797547b0b9850
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-24 07:20:34 +0300

    added helmet

--- Security-example/server.js
+++ Security-example/server.js
@@ -2,11 +2,11 @@ const fs = require("fs");
 const https = require("https");
 const path = require("path");
 const express = require("express");
-
+const helmet = require("helmet");
 const PORT = 3000;
 
 const app = express();
-
+app.use(helmet());
 app.get("/secret", (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
