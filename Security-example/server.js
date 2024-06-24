2ad9404b7e35e15f9eb49be80ea031fe48109c80
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-24 06:48:29 +0300

    add key and cert

--- Security-example/server.js
+++ Security-example/server.js
@@ -1,18 +1,28 @@
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const express = require("express");
 
 const PORT = 3000;
 
 const app = express();
 
-app.get('/secret', (req, res) => {
-  return res.send('Your personal secret value is 42!');
+app.get("/secret", (req, res) => {
+  return res.send("Your personal secret value is 42!");
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
