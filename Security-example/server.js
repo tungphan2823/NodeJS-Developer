5c310a5581798007d65edca9669fa53a419cbf45
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-25 16:45:06 +0300

    dotenv for secure

--- Security-example/server.js
+++ Security-example/server.js
@@ -3,11 +3,30 @@ const https = require("https");
 const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
+require("dotenv").config();
+
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const app = express();
 app.use(helmet());
-app.get("/secret", (req, res) => {
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).send("You are not logged in!");
+  }
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+app.get("/auth/google/callback", (req, res) => {});
+app.get("/auth/logout", (req, res) => {});
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
