c9daa5887920e067be18ae5eae0ef487d1b1baa2
Tung Phan <thanhtungphanvu@gmail.com>
2024-06-26 19:21:38 +0300

    add cookie_key

--- Security-example/server.js
+++ Security-example/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
-
+const cookieSession = require("cookie-session");
 require("dotenv").config();
 
 const PORT = 3000;
@@ -13,6 +13,8 @@ const PORT = 3000;
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 const AUTH_OPTIONS = {
   clientID: config.CLIENT_ID,
@@ -27,6 +29,15 @@ function verifyCallBack(accessToken, refreshToken, profile, done) {
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallBack));
 const app = express();
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+
 app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
