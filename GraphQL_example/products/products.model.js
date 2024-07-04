677c95b1c064cf5d79a4c7dd730a0822bb73af40
Tung Phan <thanhtungphanvu@gmail.com>
2024-07-04 03:07:06 +0300

    add new product

--- GraphQL_example/products/products.model.js
+++ GraphQL_example/products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: "readshoe",
     description: "Red Shoe",
     price: 19.99,
+    reviews: [],
   },
   {
     id: "blueshoe",
     description: "Blue Shoe",
     price: 18.99,
+    reviews: [],
   },
 ];
 function getAllProducts() {
@@ -21,4 +23,19 @@ function getProductsByPrice(min, max) {
 function getProductById(id) {
   return products.filter((product) => product.id === id);
 }
-module.exports = { getAllProducts, getProductsByPrice, getProductById };
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+  products.push(newProduct);
+  return newProduct;
+}
+module.exports = {
+  getAllProducts,
+  getProductsByPrice,
+  getProductById,
+  addNewProduct,
+};
